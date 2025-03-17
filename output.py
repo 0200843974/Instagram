a9655d790d48b76c18d48797d613fb635645248d
Shayan <mshn1398@gmail.com>
2025-03-17 20:42:45 +0330

    add layout and database to project

---
+++ output.py
@@ -0,0 +1,34 @@
+def space():
+    #This section is for line spacing.
+    print()
+
+def t_header(text):
+    #Headers should be written with this function
+    #Color of this section shold be ...
+    print(text)
+
+def t_description(text):
+    #descriptions should be written with this function
+    #Color of this section shold be ...
+    print(text)
+
+def t_select(text):
+    #select boxes should be written with this function
+    #Color of this section shold be ...
+    print(text)
+
+#messages
+def m_error(message):
+    #Errors should be written with this function
+    #Color of this section shold be red
+    print(message)
+
+def m_success(message):
+    #Successes should be written with this 
+    #Color of this section shold be green
+    print(message)
+
+def m_info(message):
+    #Informations should be written with this function
+    #Color of this section shold be blue
+    print(message)
